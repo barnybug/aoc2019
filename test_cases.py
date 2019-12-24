163c4ebfbeffbc470993be245154e95e231b3d60
Barnaby Gray <barnaby.gray@realla.co>
2019-12-24 15:03:10 +0000

    Fix imports

--- test_cases.py
+++ test_cases.py
@@ -3,26 +3,9 @@ from dataclasses import dataclass
 import numpy as np
 import pytest
 
-import day01
-import day02
-import day03
-import day04
-import day05
-import day06
-import day07
-import day08
-import day09
-import day10
-import day11
-import day12
-import day13
-import day14
-import day15
-import day16
-import day17
-import day18
-import day19
-import day20
+import day01, day02, day03, day04, day05, day06, day07, day08, day09, day10
+import day11, day12, day13, day14, day15, day16, day17, day18, day19, day20
+import day21
 from intcode import Code, Executor
 
 
